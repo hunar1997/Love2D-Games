2c28dd860df73a128e041b0c5fd0d50e2eda55fe
hunar1997 <hbkurd@gmail.com>
2017-06-06 19:16:32 +0300

    doing the actual gradient descent
    The code is rubbish now

--- gradient descent/main.lua
+++ gradient descent/main.lua
@@ -2,25 +2,37 @@ function love.load()
   debug = ''
   points = {}
 
-  learning_rate = 0.01
+  learning_rate = 0.00001
 
   sw, sh = love.graphics.getDimensions()
-
+  w1, w2 = 1, 1
   square_size = 20
+end
+function learn()
+  for i = 0, sw - square_size, square_size do
+    for j = 0, sh - square_size, square_size do
+      x = math.floor(i/square_size)
+      y = math.floor(j/square_size)
+
+      actual = screen[y*width+x]
+      error = actual + (w1*x+w2*y)
+
+      w1 = w1 + (2*error*x)*learning_rate
+      w2 = w2 + (2*error*y)*learning_rate
+    end
+  end
 end
 
 function love.update(dt)
   debug = 'Number of points: ' .. #points
-end
 
-function love.draw()
   width = sw/square_size
   screen = {}
   for i = 0, sw - square_size, square_size do
     for j = 0, sh - square_size, square_size do
       x = math.floor(i/square_size)
       y = math.floor(j/square_size)
-      screen[y*width+x] = 0
+      screen[y*width+x] = x*w1+y*w2
     end
   end
 
@@ -29,7 +41,9 @@ function love.draw()
     y = math.floor(v.y/square_size)
     screen[y*width+x] = 255
   end
+end
 
+function love.draw()
   for i = 0, sw - square_size, square_size do
     for j = 0, sh - square_size, square_size do
       x = math.floor(i/square_size)
@@ -50,6 +64,13 @@ function love.draw()
   love.graphics.setColor(55, 200, 100)
   love.graphics.setFont(love.graphics.newFont(20))
   love.graphics.print(debug, 50, 20)
+end
+
+function love.keyreleased(key)
+  if key=='l' then
+    learn()
+    print(w1, w2)
+  end
 end
 
 pressed = false
